c9e4dea2eb8e81fe08a830f4c6dfbc5080ec18e3
santosalan <9675697+santosalan@users.noreply.github.com>
2018-01-26 20:14:14 -0200

    Add files via upload

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SantosAlan\LaravelCrud;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Container\Container;
+use SantosAlan\LaravelCrud\Console\Commands\CrudMakeCommand;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProviderphp 
+{
+
+    public function boot(Factory $view, Dispatcher $events, Repository $config) 
+    {     
+        $this->registerCommands();
+
+    }
+
+    private function registerCommands()
+    {
+       
+        $this->commands(CrudMakeCommand::class);
+       
+    }
+
+}
