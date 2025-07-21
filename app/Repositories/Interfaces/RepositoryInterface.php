3cd4408868c19f8cdcd8359cd6459555f68b0373
Alan Santos <malk0704@gmail.com>
2025-07-21 06:41:19 +0100

    feat: update stubs and interface

--- app/Repositories/Interfaces/RepositoryInterface.php
+++ app/Repositories/Interfaces/RepositoryInterface.php
@@ -2,11 +2,14 @@
 
 namespace App\Repositories\Interfaces;
 
+use Illuminate\Database\Eloquent\Builder;
+
 interface RepositoryInterface
 {
+    public function newObject();
     public function plucks();
     public function all();
-    public function filter(array $filters);
+    public function filter(array $filters, ?Builder $builder = null);
     public function create(array $data);
     public function find($id);
     public function update($id, array $data);
